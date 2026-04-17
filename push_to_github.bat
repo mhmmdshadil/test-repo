8c333f174a3a3af24aac9a8a8d371093c826286b
mhmmdshadil <mhmmdshadil@github.com>
2026-04-17 08:05:06 +0530

    chore: push all latest changes

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,10 +1,5 @@
 @echo off
 set GIT="C:\Program Files\Git\cmd\git.exe"
-%GIT% config --global user.email "mhmmdshadil@github.com"
-%GIT% config --global user.name "mhmmdshadil"
-%GIT% add .
-%GIT% commit -m "Initial commit: Red Pulse Emergency ID system"
-%GIT% branch -M main
-%GIT% remote remove origin
-%GIT% remote add origin https://github.com/mhmmdshadil/test-repo.git
-%GIT% push -u origin main --force
+%GIT% add -A
+%GIT% commit -m "chore: push all latest changes"
+%GIT% push origin main
